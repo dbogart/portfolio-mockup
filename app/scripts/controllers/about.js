95e182cd16d5dd8353e5136abe6b0e691e59b75d
dbogart <dbogart@adverseevents.com>
2014-11-19 09:55:51 -0800

    inital commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name portfolioMockupApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the portfolioMockupApp
+ */
+angular.module('portfolioMockupApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name portfolioMockupApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the portfolioMockupApp
+ */
+angular.module('portfolioMockupApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
