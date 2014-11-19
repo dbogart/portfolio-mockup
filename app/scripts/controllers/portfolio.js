9da91ef3cb75d6070222c25ed1ceadd9b04ea710
dbogart <dbogart@adverseevents.com>
2014-11-19 14:05:25 -0800

    moved repeated lines to ctrl and used directives

--- app/scripts/controllers/portfolio.js
+++ app/scripts/controllers/portfolio.js
@@ -9,9 +9,37 @@
  */
 angular.module('portfolioMockupApp')
   .controller('PortfolioCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+    $scope.portItems = [
+      'Actavis',
+      'Arava',
+      'Avacor',
+      'Avagard'
     ];
+    $scope.companies = [
+      'Regeneron',
+      'Biogen',
+      'Celgene'
+    ];
+    $scope.drugs = [
+      'Avandia',
+      'Avastin',
+      'Ambien'
+    ];
+    $scope.events = [
+      { product: 'Avastin', headline: 'Docetaxel Drug Label Update: A Comparative Safety Analysis' },
+      { product: 'Arava', headline: 'AEI Analysis Produces Skin Cancer Safety Signal for Xeljanz Prior to FDA Safety Alert' },
+      { product: 'Ambien', headline: 'Safety Labeling Changes' }
+    ];
+	$scope.addToPortfolio = function (index) {
+        $scope.companies.push($scope.portItems[index]);
+        $scope.portItems.splice(index, 1);
+    };
+	$scope.removeFromPortfolio = function (index) {
+        $scope.portItems.push($scope.companies[index]);
+        $scope.companies.splice(index, 1);
+    };
+	$scope.removeDrugFromPortfolio = function (index) {
+        $scope.portItems.push($scope.drugs[index]);
+        $scope.drugs.splice(index, 1);
+    };
   });
