33277cffbed875760482ab526ca88e5f4ac50124
dbogart <dbogart@adverseevents.com>
2014-11-26 07:21:55 -0800

    dropdown updates

--- app/scripts/controllers/dropdown.js
+++ app/scripts/controllers/dropdown.js
@@ -5,29 +5,13 @@ angular.module('portfolioMockupApp').controller('DropdownCtrl', function ($scope
   
   $scope.disableDropdown = portfolioCreateService.getDropdownClass();
   
-  if ($scope.disableDropdown === 'true') {
-    $scope.dropdownClass = 'disabled-dropdown';
-  } else {
-      $scope.dropdownClass = 'false';
-  }
-
   $scope.$on('dropdownStatus', function(){
     $scope.disableDropdown = portfolioCreateService.getDropdownClass();
-    if ($scope.disableDropdown === 'true') {
-      $scope.dropdownClass = 'disabled-dropdown';
-      console.log('updated');
-    } else {
-      $scope.dropdownClass = 'false';
-    }
   });
 
   $scope.status = {
     isopen: false
   };
-
-  // $scope.toggled = function(open) {
-  //   $log.log('Dropdown is now: ', open);
-  // };
 
   $scope.toggleDropdown = function($event) {
     $event.preventDefault();
